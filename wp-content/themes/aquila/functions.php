2bcf6c0e821323a0e49daafcd2eee0e51e04c5ba
Afaq <afaqnaveed@gmail.com>
2025-01-29 04:43:28 +0500

    Create folders inc/helpers added autoloader.php | Created folder /inc/traits added trait-singleton.php | Created a constant AQUILA_THEME_PATH in functions.php | Included autoloader.php using the constant in functions.php

--- wp-content/themes/aquila/functions.php
+++ wp-content/themes/aquila/functions.php
@@ -5,6 +5,12 @@
  * @package Aquila
  */
 
+if ( ! defined( 'AQUILA_DIR_PATH' ) ) {
+    define( 'AQUILA_DIR_PATH', untrailingslashit(get_template_directory(  ) ) );
+}
+
+require_once AQUILA_DIR_PATH . '/inc/helpers/autoloader.php';
+
 function aquila_enqueue_scripts() {
     /**wp_enqueue_style('style-css', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css'), 'all' );
     wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime( get_template_directory() . '/assets/main.js'), true );*/
